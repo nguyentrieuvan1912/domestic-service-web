9f7661494ab06c1363d7b258effe42c1b0930e58
Van1912 <nguyentrieuvan19122003@gmail.com>
2026-09-05 14:49:53 +0700

    init project

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="flex h-screen items-center justify-center bg-blue-50">
+      <h1 className="text-4xl font-bold text-blue-600">
+        Khởi tạo Web Thành Công!
+      </h1>
+    </div>
+  )
+}
+
+export default App
